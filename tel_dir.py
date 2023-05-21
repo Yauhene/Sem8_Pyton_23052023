f15d397bdd4f88805b7639399af3a708bd48931c
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-05-21 21:33:48 +0300

    readFile added

--- tel_dir.py
+++ tel_dir.py
@@ -1,10 +1,27 @@
-﻿
 
-getOut = False
+
+#getOut = False
 # === функция чтения файла ===========================================
 def readFile(fName):
     print()
-    # пока пусто
+    with open(fName, 'r', encoding='utf-8') as file:
+        print()
+        fileContent = list()
+        for line in file.readlines():
+            fileContent.append((tuple(line.split('\n')[0].split(';'))))
+        for el in fileContent:
+            for i in range(0,len(el)):
+                print(el[i], ' ', end = '')
+            print()
+        #print('Поиграемся!')
+        # for i in range(0,len(fileContent)):
+        #     for j in range(0,len(fileContent[i])):
+        #         print(fileContent[i][j], " ", end='')
+        #     print()
+        #print(fileContent)
+
+    #input()
+    return fileContent
 
 # === функция сохранения файла ===========================================
 def saveFile(fName):
@@ -19,8 +36,9 @@ def findAbon(findArg):
 # === функция поиска абонента ===========================================
 def mainMenu():
     getOut=False
+    
 
-    while getOut:
+    while getOut != True:
         print()
         print('=========================================================')
         print('Выберите пункт меню, нажав соответствующую цифру и Enter.')
@@ -32,10 +50,18 @@ def mainMenu():
         print('Q/q. выйти из программы')
         print()
         choice = input('Ваш выбор: ')
-        if choice == 'Q' or 'q': getOut=True
-        # пока пусто
+        #print(f'choice = {choice}')
+        if choice == '1':
+            readFile('telephone_direct.txt')
+            
+        if choice == 'Q' or choice == 'q': 
+            getOut=True
+            print()
+            print('Дело хозяйское... До новых встреч!')
+            print()
 
 def main():
     mainMenu()
 
-mainMenu() # старт программы -------------------------------------------------------------------
+#main() # старт программы -------------------------------------------------------------------
+mainMenu()
